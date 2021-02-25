66c929cdf431ecff74af50874a5477ae719e01f8
Jacob Huggins <hugginsjw@g.cofc.edu>
2021-02-25 18:03:08 -0500

    Activity 1 Layout

--- app/src/main/java/com/example/tipcalculator_huggins/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_huggins/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.tipcalculator_huggins;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextTip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editTextTip = findViewById(R.id.editTextTip);
+    }
+
+    public void onRadioButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButtonCustom:
+                if(checked){ editTextTip.setEnabled(true); }
+                break;
+            default:
+                editTextTip.setEnabled(false);
+                break;
+        }
     }
 }
